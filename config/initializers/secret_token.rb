060f917feda61c629f5833fd97603f32c3e372e6
Innocent Ekeh <Innoekeh@gmail.com>
2014-01-27 22:03:02 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Raredone::Application.config.secret_key_base = '057e7ac849f9467c047c8726caad9942621682dbe538603459c8e7af6df732ea0c8c1eaf101befadee5451294292a1e0e95ef49853b79287d44392737d562d15'
